9cbd351dd02b4a90ff23155c14b71a1b987d0072
Santhosh Jackson <santhosh.jack.j@gmail.com>
2019-01-08 15:38:16 +0000

    Repo Test Case changes

--- candidateTracker/src/test/java/com/tracker/tests/CandidateRepoTests.java
+++ candidateTracker/src/test/java/com/tracker/tests/CandidateRepoTests.java
@@ -2,9 +2,11 @@ package com.tracker.tests;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,11 +21,21 @@ import com.tracker.repo.CandidateRepo;
 @DataJpaTest
 public class CandidateRepoTests {
 
+	private static final String QUALIFICATION = "Qualification";
+
+	private static final String PROFILE_LINK = "profileLink";
+
+	private static final String NAME = "Name";
+
+	private static final String INTERESTS = "Interests";
+
+	private static final long CANDIDATE_ID = 100l;
+
 	@Autowired
 	private CandidateRepo candidateRepo;
 
 	@Test
-	public void checkGetAllCandidateTest() {
+	public void findAllTest() {
 
 		List<Candidate> candidates = new ArrayList<>();
 		Iterable<Candidate> candidateData = candidateRepo.findAll();
@@ -32,4 +44,42 @@ public class CandidateRepoTests {
 		assertEquals(4, candidates.size());
 
 	}
+
+	@Test
+	public void findByIdTest() {
+		Optional<Candidate> candidate = candidateRepo.findById(CANDIDATE_ID);
+		assertTrue(candidate.isPresent());
+	}
+
+	@Test
+	public void saveTest() {
+		Candidate candidate = getCandidate();
+
+		Candidate savedInfo = candidateRepo.save(candidate);
+		assertNotNull(savedInfo.getId());
+		assertEquals(NAME, candidate.getName());
+	}
+
+	@Test
+	public void updateTest() {
+		Candidate candidate = candidateRepo.findById(CANDIDATE_ID).get();
+		candidate.setName(NAME);
+		Candidate updatedCandidate = candidateRepo.save(candidate);
+		assertEquals(NAME, updatedCandidate.getName());
+	}
+
+	@Test
+	public void deleteTest() {
+		candidateRepo.deleteById(CANDIDATE_ID);
+	}
+
+	private Candidate getCandidate() {
+		Candidate candidate = new Candidate();
+		candidate.setInterests(INTERESTS);
+		candidate.setName(NAME);
+		candidate.setProfileLink(PROFILE_LINK);
+		candidate.setQualification(QUALIFICATION);
+		return candidate;
+	}
+
 }
